4c307e582ec41bfe24ed02f4a653a546d7ae1d70
9344642 <9344642@gmail.com>
2026-03-09 00:36:39 +0000

    fix: восстановить framework vite + убрать transparent

--- src/main.js
+++ src/main.js
@@ -20,7 +20,6 @@ const phaserConfig = {
   width:  GAME_CONFIG.width,
   height: GAME_CONFIG.height,
   parent: 'game-container',
-  transparent: true,
   scale: {
     mode:            Phaser.Scale.FIT,
     autoCenter:      Phaser.Scale.CENTER_BOTH,
